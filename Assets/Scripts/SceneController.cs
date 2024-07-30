42fb5f3d553a23bb59c04a9de858c9b71e57c20d
Khadija Saeed <khadijasaeed683@gmail.com>
2024-07-31 01:19:01 +0500

    31-7

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -1,22 +1,36 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
 {
-
+   
     // Method to load a scene by name
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+
+    }
+
     public void LoadSceneByName(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
     }
+
     public void LoadLevel1()
     {
         SceneManager.LoadSceneAsync("Level1");
     }
+
     public void CloseGame()
     {
         Application.Quit();
     }
+
+   
+  
+   
 }
